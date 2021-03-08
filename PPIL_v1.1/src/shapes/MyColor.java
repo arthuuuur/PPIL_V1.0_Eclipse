bbb40a2e6921bd04a64cb602319091472a731f41
Arthuuuur <arthur.troyon3@etu.univ-lorraine.fr>
2021-03-09 00:27:11 +0100

    ajout map color

--- PPIL_v1.1/src/shapes/MyColor.java
+++ PPIL_v1.1/src/shapes/MyColor.java
@@ -1,44 +1,32 @@
 package shapes;
 
 import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This class is usefull to convert a color from string format to awt.Color
  */
 public class MyColor {
     
-	/**
-	 * The converted color
-	 */
-	private Color color = null;
+	private static Map<String, Color> availableColor = new HashMap<String, Color>();
+	static {
+		availableColor.put("black", Color.BLACK);
+		availableColor.put("red", Color.RED);
+		availableColor.put("green", Color.GREEN);
+		availableColor.put("cyan", Color.CYAN);
+		availableColor.put("blue", Color.BLUE);
+		availableColor.put("yellow", Color.YELLOW);
+	}
 
 	/**
-	 * Allow to convert a color from string format to awt.Color
+	 * Allow to convert a color from string format to awt.Color thanks to the Map
 	 * 
 	 * @param col The color in string format to be converted in awt.Color
 	 * @return the converted color
 	 */
 	public Color getColor(String col) {
-	    switch (col.toLowerCase()) {
-	    case "black":
-	        color = Color.BLACK;
-	        break;
-	    case "blue":
-	        color = Color.BLUE;
-	        break;
-	    case "green":
-	        color = Color.GREEN;
-	        break;
-	    case "yellow":
-	        color = Color.YELLOW;
-	        break;
-	    case "red":
-	        color = Color.RED;
-	        break;
-	    case "cyan":
-	        color = Color.CYAN;
-	        break;
-	    }
-		return color;
+	    col.toLowerCase();
+	    return availableColor.get(col);
 	}
 }
